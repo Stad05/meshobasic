a29c82389803c0fabf284a2b488bd58acb3f2462
Admin <Admin@DESKTOP-K5LDBGH>
2025-08-13 15:24:25 +0530

    printhello project added

---
+++ Printhello/src/stad/child.java
@@ -0,0 +1,10 @@
+package stad;
+
+public class child {
+	
+	void test()
+	{
+		System.out.println("in test");
+	}
+
+}
---
+++ Printhello/src/stad/printhello.java
@@ -0,0 +1,16 @@
+package stad;
+
+public class printhello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		String s= "hello";
+		int a = s.length();
+		System.out.println(a);
+		
+		child c = new child();
+		c.test();
+	}
+
+}
